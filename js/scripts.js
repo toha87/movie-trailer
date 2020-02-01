4cba3342ef6804388299be8ecbf330f9920de808
Anatoli Railean <arailean@eagles.ewu.edu>
2020-02-01 00:43:37 -0800

    Added params for player

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ console.log('OK');
 // Initialize player
 const player = new Plyr('#player', {
 
-    controls: [ 'play', 'current-time', 'progress' ],
+    controls: ['play', 'current-time', 'progress', 'volume'],
+    autoplay: true,
+    muted: true,
+    loop: { active: true }
 
 });
