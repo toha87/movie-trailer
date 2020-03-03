8a5fdd3ed35fa26011b0c8e4518a8acd6ba4f937
Anatoli Railean <arailean@eagles.ewu.edu>
2020-03-02 21:41:37 -0800

    Added event listeners for buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 console.log('OK');
 
+let prevLinkId;
+
 // Initialize player
 const player = new Plyr('#player', {
 
@@ -9,3 +11,21 @@ const player = new Plyr('#player', {
     loop: { active: true }
 
 });
+
+let linkArray = ["#navLinkHome", "#navLinkInfo", "#navLinkCast", "#navLinkLinks"];
+
+for (const navLink of linkArray) {
+
+    let linkElement = document.querySelector(navLink);
+
+    linkElement.addEventListener('click', (event) => {
+
+        if (prevLinkId) {
+            document.querySelector("#" + prevLinkId).classList.remove("active");
+        }
+
+        prevLinkId = event.target.id;
+
+        linkElement.classList.add("active");
+    });
+}
