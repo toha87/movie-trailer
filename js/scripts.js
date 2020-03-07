942a9b6b77d7999d71574d9fb5a5895f0b2f4941
Anatoli Railean <arailean@eagles.ewu.edu>
2020-03-07 12:05:25 -0800

    Added Splitting.js && GSAP

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,17 @@ for (const navLink of linkArray) {
         linkElement.classList.add("active");
     });
 }
+
+
+let split = Splitting({
+    target: '#info'
+});
+
+
+var tl = gsap.timeline(),
+
+    chars = split[0].chars;
+
+gsap.set("#info", { perspective: 300 });
+
+tl.from(chars, { duration: 10, opacity: 0, scale: 0, y: 80, rotationX: 180, transformOrigin: "0% 50% -50", ease: "back", stagger: 0.01 }, "+=0");
